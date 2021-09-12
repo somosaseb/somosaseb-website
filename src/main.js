fc713e305e54cc7e3e562d75c34d503034566fe3
Mario César <mariocesar.c50@gmail.com>
2021-09-12 04:27:40 -0400

    jsconfig and import paths

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import DefaultLayout from "~/layouts/Default.vue";
-import Banner from "~/components/Banner.vue";
-import Pricing from "~/components/Pricing.vue";
-import HeroText from "~/components/HeroText.vue";
-import HeroImage from "~/components/HeroImage.vue";
-import TextHeading from "~/components/TextHeading.vue";
-import TextLead from "~/components/TextLead.vue";
-import BlogSummary from "~/components/BlogSummary.vue";
-import NewsLink from "~/components/NewsLink.vue";
-import EventSummary from "~/components/EventSummary.vue";
-import StaffList from "~/components/StaffList.vue";
-import Faq from "~/components/Faq.vue";
+import DefaultLayout from "./layouts/Default.vue";
+import Banner from "./components/Banner.vue";
+import Pricing from "./components/Pricing.vue";
+import HeroText from "./components/HeroText.vue";
+import HeroImage from "./components/HeroImage.vue";
+import TextHeading from "./components/TextHeading.vue";
+import TextLead from "./components/TextLead.vue";
+import BlogSummary from "./components/BlogSummary.vue";
+import NewsLink from "./components/NewsLink.vue";
+import EventSummary from "./components/EventSummary.vue";
+import StaffList from "./components/StaffList.vue";
+import Faq from "./components/Faq.vue";
 
 import "./assets/scss/main.scss";
 
@@ -26,9 +26,9 @@ export default function(Vue, { router, head, isClient }) {
       document.querySelector(selector).classList.toggle(className);
     },
   };
+
   Vue.component("Layout", DefaultLayout);
   Vue.component("BlogSummary", BlogSummary);
-
   Vue.component("Banner", Banner);
   Vue.component("Pricing", Pricing);
   Vue.component("HeroText", HeroText);
